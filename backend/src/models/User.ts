4fa4868a5e49400d28c95d105340677df16d3ee5
Leo Gyger <lgyger@c3r4s2.42lausanne.ch>
2022-06-13 12:38:08 +0200

    I think it works now, I hope at least

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,1 +1,15 @@
-import { Sequelize, DataTypes } from "sequelize";
+import pkg from "pg";
+const { Client } = pkg;
+const func = (id, login, displayname, image_url) => {
+  const client = new Client({
+    user: process.env.DB_USER,
+    host: "localhost",
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+  });
+  client.connect();
+  client.query(`insert into users (id, login, displayname, image_url)
+    values (${id}, ${login}, ${displayname}, ${image_url})`);
+};
+func(103, "lgyger", "Leo Gyger", "test");
